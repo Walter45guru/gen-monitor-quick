23fb7ac539772d540b15dbe7a9b02ef44a6b050b
Walter45guru <walter.ouma@strathmore.edu>
2025-06-09 09:20:39 +0300

    chore: remove backend from main branch, frontend only

--- generator_backend/build_files.sh
+++
@@ -1,6 +0,0 @@
-# build_files.sh
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput 
--- generator_backend/data_processor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/generator-data/', views.receive_generator_data, name='receive_generator_data'),
-    path('test-db/', views.test_db_connection, name='test_db_connection'),
-] 
