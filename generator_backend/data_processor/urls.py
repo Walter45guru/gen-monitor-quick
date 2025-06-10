61e51f117de0b9db6bcc1635ffe9a9e35553f209
Walter45guru <walter.ouma@strathmore.edu>
2025-06-10 08:57:49 +0300

    fix: add receive_generator_data endpoint for test script and dashboard API

--- generator_backend/data_processor/urls.py
+++ generator_backend/data_processor/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('api/receive-generator-data/', views.receive_generator_data, name='receive_generator_data'),
     path('api/generator-data-csv/', views.generator_data_csv, name='generator_data_csv'),
     path('api/latest-generator-data/', views.get_latest_generator_data, name='get_latest_generator_data'),
 ] 
